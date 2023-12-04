28ec5e91af7bbae3c018d85d890d28cb8da25582
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2023-12-04 07:46:45 -0500

    Update - React

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './Assets/React.svg'
+import viteLogo from '../Public/Vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
