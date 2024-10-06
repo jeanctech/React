65425da422c95323959ee2c810246a5cdc749ce6
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2024-10-06 10:45:19 -0500

    Changes Files - Rc

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -15,23 +15,23 @@ function App() {
             React.js
           </h1>
           <a href="https://vitejs.dev">
-            <img src={vite} className="m-1 p-1" alt="Vite-Logo" width={75} />
+            <img src={vite} id='logo-vite' className="m-1 p-1" alt="Vite-Logo" width={50} />
           </a>
           <a href="https://reactjs.dev">
-            <img src={react} className="m-1 p-1" alt="React-Logo" width={75} />
+            <img src={react} id='logo-react' className="m-1 p-1" alt="React-Logo" width={50} />
           </a>
         </div>
         <div id="app-card"
           className="m-2.5 p-2.5 flex flex-row justify-center items-center" >
-          <button onClick={() => setCount((count) => count + 1)} >
+          <button className="m-2.5 p-2.5 bg-[#23383b]" onClick={() => setCount((count) => count + 1)} >
             Count Is - {count}
           </button>
-          <p className="m-2.5 p-2.5 bg-[#1A1A21] text-slate-300 rounded-xl" >
-            Edit <code className="m-1 p-1 bg-[#15263b] rounded-lg">./src/components/app.jsx</code> and save to test Hmr
+          <p className="m-2.5 p-2.5 bg-[#2d3f42] text-slate-300 rounded-xl" >
+            Edit <code className="m-1 p-1 font-bold text-[#00D8FF]">./src/components/app.jsx</code> and Save to Test Hmr
           </p>
         </div>
-        <p id="read" className="m-2.5 p-2.5 bg-[#1A121A] rounded-lg"  >
-          Click on the Vite and React Logos to learn more
+        <p id="read" className="m-2.5 p-2.5 bg-[#3c4446] rounded-lg"  >
+          Click on The Vite and React Logos to Learn More
         </p>
       </div>
     </>
